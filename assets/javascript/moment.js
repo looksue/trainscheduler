b47547780dcc17a93cabf303379ab0c0b0b880e0
Sue Look <suelook1@gmail.com>
2019-08-24 14:49:27 -0400

    added form box with submit btn

---
+++ assets/javascript/moment.js
@@ -0,0 +1,11 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyCL8jfBgi2tatVsYihQ_dns1E2xInzsN2s",
+    authDomain: "choochootrainscheduler.firebaseapp.com",
+    databaseURL: "https://choochootrainscheduler.firebaseio.com",
+    projectId: "choochootrainscheduler",
+    storageBucket: "",
+    messagingSenderId: "344520636909",
+    appId: "1:344520636909:web:6bf23b67e7781acc"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
