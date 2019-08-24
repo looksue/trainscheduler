1c9057c4248597feb1125a9a4b95fd7c1152e208
Sue Look <suelook1@gmail.com>
2019-08-24 19:29:58 -0400

    added firebase child snapshot and screen update code

--- assets/javascript/trainschedule.js
+++ assets/javascript/trainschedule.js
@@ -24,8 +24,49 @@ $("#btnSubmit").on("click", function(event) {
   var trainFrequency = $("#txtFrequency").val().trim();
 
   //create local vars for train data
+  var recAddTrain = {
+  colTrainName: trainName,
+  colDestination: trainDestination,
+  colFirstTrain: trainFirstTrainTime,
+  colFrequency: trainFrequency,
+  };
 
   //upload train data to database
+  database.ref().push(recAddTrain);
 
   //clear all text boxes
+  $("#txtTrainName").val("");
+  $("#txtDestination").val("");
+  $("#txtFirstTrainTime").val("");
+  $("#txtFrequency").val("");
+
+});
+
+//create firebase event a train to the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot) {
+
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().colTrainName;
+    var trainDestination = childSnapshot.val().colDestination;
+    var trainFirstTrainTime = childSnapshot.val().colFirstTrain;
+    var trainFrequency = childSnapshot.val().colFrequency;
+  
+    // Calculate next arrival and minutes away
+   // var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
+    //var empMonths = moment().diff(moment(empStart, "X"), "months");
+    var trainNextArrival;
+    var trainMinutesAway;
+  
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDestination),
+      $("<td>").text(trainFrequency),
+      $("<td>").text(trainNextArrival),
+      $("<td>").text(trainMinutesAway)
+    );
+  
+    // Append the new row to the table
+    $("#tblCurrentTrain > tbody").append(newRow);
+  
 });
