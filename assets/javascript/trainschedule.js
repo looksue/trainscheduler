98ec94e1db5212998c13cec625958622f31bfd94
Sue Look <suelook1@gmail.com>
2019-08-27 12:46:29 -0400

    fixed divs tags and listener working

--- assets/javascript/trainschedule.js
+++ assets/javascript/trainschedule.js
@@ -1,4 +1,5 @@
-  var firebaseConfig = {
+ 
+ var firebaseConfig = {
     apiKey: "AIzaSyCL8jfBgi2tatVsYihQ_dns1E2xInzsN2s",
     authDomain: "choochootrainscheduler.firebaseapp.com",
     databaseURL: "https://choochootrainscheduler.firebaseio.com",
@@ -14,20 +15,23 @@ firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
 //adding event listener for the submit button
-$("#btnSubmit").on("click", function(event) {
+$("#btnSubmit").on("click", function() {
+  alert("hey it doesn't work");
   event.preventDefault();
+  console.log("Here I am!");
   
   //grab user input
   var trainName = $("#txtTrainName").val().trim();
   var trainDestination = $("#txtDestination").val().trim();
   var trainFirstTrainTime = $("#txtFirstTrainTime").val().trim();
   var trainFrequency = $("#txtFrequency").val().trim();
-
+  console.log(trainName);
+  alert(trainName);
   //create local vars for train data
   var recAddTrain = {
   colTrainName: trainName,
   colDestination: trainDestination,
-  colFirstTrain: trainFirstTrainTime,
+  colFirstTrainTime: trainFirstTrainTime,
   colFrequency: trainFrequency,
   };
 
@@ -42,20 +46,48 @@ $("#btnSubmit").on("click", function(event) {
 
 });
 
+
 //create firebase event a train to the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot) {
 
     // Store everything into a variable.
     var trainName = childSnapshot.val().colTrainName;
     var trainDestination = childSnapshot.val().colDestination;
-    var trainFirstTrainTime = childSnapshot.val().colFirstTrain;
+    var trainFirstTrainTime = childSnapshot.val().colFirstTrainTime;
     var trainFrequency = childSnapshot.val().colFrequency;
   
     // Calculate next arrival and minutes away
    // var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
     //var empMonths = moment().diff(moment(empStart, "X"), "months");
     var trainNextArrival;
-    var trainMinutesAway;
+    var trainfrequency;
+    //once a minute .....update 
+    //need the first train time, add the frequency until greater than current time
+    var firstTrainTimeConverted = moment(trainFirstTrainTime, "hh:mm");
+    var trainMinutesAway = moment().diff(moment(firstTrainTimeConverted), "minutes");
+    while (trainMinutesAway < 0) {
+      firstTrainTimeConverted = firstTrainTimeConverted + moment(trainFrequency, "mm");
+      trainMinutesAway = moment().diff(moment(firstTrainTimeConverted), "minutes");
+    }
+
+
+//hopefully some helpful code??????
+
+   // var tRemainder = diffTime % frequency;
+   // console.log(tRemainder);
+   // var minutesTillTrain = frequency - tRemainder;
+   // console.log(minutesTillTrain);
+    // var nextTrain = firstTrainConverted.add(diffTime + minutesTillTrain).minutes();
+    //var nextTrain = moment().add(minutesTillTrain, "minutes");
+    //var nextTrain2 = moment().add(2, "minutes")
+    //console.log("num1" + nextTrain);
+    //console.log("num2" + nextTrain2);
+    //nextTrain = moment(nextTrain).format("HH:mm");
+    //console.log("num1" + nextTrain);
+
+
+    //take the resulting time and calculate how many minutes away
+    //do the same thing for the next train in the data
   
     // Create the new row
     var newRow = $("<tr>").append(
